19c1b2790972b34b5ba8b4d41ef3757b9ed63029
Diego Cando <info@inc205.com>
2021-05-23 18:30:38 -0500

    bug

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@ include_once('/Tools/config.php');
 include_once('/Model/alumno.php');
 include_once('/Model/tutor.php');
 
+$alumno = Alumno->get(0,$db);
+$alumno->nombre_alumno="Juan";
+$alumno->email_alumno="juan"
+$alumno->password="123";
+$alumno->save($db);
 echo "Hello world";
 
 ?>
