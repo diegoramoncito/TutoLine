4cfb0025a1a856295b0c5806b5f15973bda3374d
Diego Cando <info@inc205.com>
2021-05-23 02:56:01 -0500

    bug

--- public/Controller/default.php
+++ public/Controller/default.php
@@ -1,7 +1,7 @@
 <?php
-include_once('Tools/config.php');
-include_once('Model/alumno.php');
-include_once('Model/tutor.php');
+include_once('/Tools/config.php');
+include_once('/Model/alumno.php');
+include_once('/Model/tutor.php');
 
 $destination = "";
 $ruta=intval($_GET['route1']);
