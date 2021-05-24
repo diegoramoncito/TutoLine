b16de4365e8ac80d765fed315e3aec5f921d2569
Diego Cando <info@inc205.com>
2021-05-23 20:31:39 -0500

    bug

--- public/index.php
+++ public/index.php
@@ -3,12 +3,15 @@ include_once('/Tools/config.php');
 include_once('/Model/alumno.php');
 include_once('/Model/tutor.php');
 
-$result = Alumno->search("email","andre",$db);
-$alumno = Alumno->get(0,$db);
+//$result = Alumno->search("email","andre",$db);
+//$alumno = Alumno->get(0,$db);
 // $alumno->nombre_alumno="Juan";
 // $alumno->email_alumno="juan"
 // $alumno->password="123";
 // $alumno->save($db);
+
+$result = $db->fetchAll("Select * from alumnos where email like '%1%'");
+print_r($result);
 echo "Hello world";
 
 ?>
