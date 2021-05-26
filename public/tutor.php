5732a1b335bb78eb756e53c1c8005631910fbcd2
Diego Cando <info@inc205.com>
2021-05-26 17:55:16 -0500

    bug

--- public/tutor.php
+++ public/tutor.php
@@ -53,7 +53,7 @@ foreach($result as $element){
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.html" class="brand-link">
-      <img src="img/AdminLTELogo.png"class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="/img/AdminLTELogo.png"class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">TutoLine</span>
     </a>
 
@@ -62,7 +62,7 @@ foreach($result as $element){
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="img/user2-160x160.jpg" class="img-circle elevation-2">
+          <img src="/img/user2-160x160.jpg" class="img-circle elevation-2">
         </div>
         <div class="nav-header">
           <?php 
