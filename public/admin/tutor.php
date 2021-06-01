efd5471d974f682e261fabf89787db37f544ee1c
Diego Cando <info@inc205.com>
2021-06-01 18:22:46 -0500

    bug

--- public/admin/tutor.php
+++ public/admin/tutor.php
@@ -209,7 +209,7 @@ if($id!=0){
                         <?php
                         $result = $db->fetchAll("select * from categorias");
                         foreach($result as $element){?>
-                        <option value="<?php echo $element['id_categoria']; if($categoria == $element['categorias_id_categoria']) echo "selected";?>"><?php echo $element['nombre_categoria']; ?></option>
+                        <option value="<?php echo $element['id_categoria']; if($categoria == $element['categorias_id_categoria']) echo "selected";?>"><?php echo $element['nombre_categoria']."(".$element['dificultad'].")"; ?></option>
                         <?php } ?>
                     </select>
                     
