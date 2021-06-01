8e0b659f8862f4105a7820613cb280321212d499
Diego Cando <info@inc205.com>
2021-06-01 18:40:52 -0500

    bug

--- public/admin/alumno.php
+++ public/admin/alumno.php
@@ -120,15 +120,7 @@ if($id!=0){
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Perfil</h1>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+    
 
     <!-- Main content -->
     <section class="content">
