43a05b7b77b7be971d6b27c6e6cb317991956ce3
Diego Cando <info@inc205.com>
2021-06-05 20:47:25 -0500

    fix

--- public/Model/tarea.php
+++ public/Model/tarea.php
@@ -29,14 +29,12 @@ class Tarea{
                 $this->tutor_id_tutor = $element['tutor_id_tutor'];
             }
         }else{
-            $query = "insert into tareas(nombre_tarea,descripcion_tarea,calificacion_tarea,comentarios_tarea,entregable_tarea)values('','',1,'','')";
+            $query = "insert into tareas(nombre_tarea,descripcion_tarea,calificacion_tarea,comentarios_tarea,entregable_tarea,estado_tarea)values('','',1,'','','')";
             $db->execute($query);
-            error_log($query);
             $result = $db->fetchAll('SELECT LAST_INSERT_ID()');
             foreach($result as $element){
                 $this->id_tarea = $element['LAST_INSERT_ID()'];
             }
-            error_log($this->id_tarea);
         }
     }
 
